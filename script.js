a79dd67352d392a3354f3aaa9b53c8dc39b930cb
tknomnto <tknomnto020323@gmail.com>
2023-03-10 17:56:44 +0900

    データのやり取りを行ってみた

--- script.js
+++ script.js
@@ -39,26 +39,64 @@ function getFromClient(request, response){
     }
 }
 
-//追加するデータ用変数
-var data = {
-    'Taro': '09-999-999',
-    'Hanako': '080-888-888',
-    'Sachiko': '070-777-777',
-    'Ichiro': '060-666-666'
-};
+//データ
+var data = { msg: 'no message...' };
 
 //indexのアクセス処理
 function response_index(request, response){
-    var msg = "これはIndexページです。";
+    //POSTアクセス時の処理
+    if(request.method == 'POST'){
+        var body = '';
+
+        //データ受信のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        //データ受信終了のイベント処理
+        request.on('end', () => {
+            data = qs.parse(body); //データのパース
+            //クッキーの保存
+            setCookie('msg', data.msg, response);
+            write_index(request, response);
+        });
+    }else{
+        write_index(request, response);
+    }
+}
+
+//indexの表示の作成
+function write_index(request, response){
+    var msg = "※伝言を表示します。";
+    var cookie_data = getCookie('msg', request);
     var content = ejs.render(index_page, {
         title: "Index",
         content: msg,
         data: data,
-        filename: 'data_item',
+        cookie_data: cookie_data,
     });
-    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(content);
     response.end();
+}
+
+//クッキーの値を設定
+function setCookie(key, value, response){
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+
+//クッキーの値を取得
+function getCookie(key, request){
+    var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for(var i in data){
+        if(data[i].trim().startsWith(key + '=')){
+            var result = data[i].trim().substring(key.length + 1);
+            return unescape(result);
+        }
+    }
+    return '';
 }
 
 //追加するデータ用変数
@@ -73,36 +111,13 @@ data2 = {
 function response_other(request, response){
     var msg = "これはOtherページです。";
 
-    //POSTアクセス時の処理
-    if(request.method == 'POST'){
-        var body = '';
-
-        //データ受信のイベント処理
-        request.on('data', (data) => {
-            body += data;
-        });
-
-        //データ受信終了のイベント処理
-        request.on('end', () => {
-            var post_data = qs.parse(body); //データのパース
-            msg += 'あなたは、「' + post_data.msg + '」と書きました。';
-            var content = ejs.render(other_page, {
-                title: "Other",
-                content: msg,
-            });
-            response.writeHead(200, {'Content-Type' : 'text/html'});
-            response.write(content);
-            response.end();
-        })
-        //Getアクセス時の処理
-    } else {
-        var msg = "ページがありません。";
-        var content = ejs.render(other_page, {
-            title: "Other",
-            content: msg,
-        });
-        response.writeHead(200, {'Content-Type' : 'text/html'});
-        response.write(content);
-        response.end();
-    }
+    var content = ejs.render(other_page, {
+        title: "Other",
+        content: msg,
+        data: data2,
+        filename: 'data_item',
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end();
 }
